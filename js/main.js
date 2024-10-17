92f9845f1761687d3e8f4d6a92a2a8afe357ec11
webexpert123 <webexpert987@gmail.com>
2024-10-17 17:29:32 +0530

    home page recreation

--- js/main.js
+++ js/main.js
@@ -13,3 +13,15 @@ document.addEventListener("click", (event) => {
         menuClicked = false;
     }
 });
+/**
+   * Apply .scrolled class to the body as the page is scrolled down
+   */
+function toggleScrolled() {
+    const selectBody = document.querySelector('body');
+    const selectHeader = document.querySelector('.header-main');
+    if (!selectHeader.classList.contains('scroll-up-sticky') && !selectHeader.classList.contains('sticky-top') && !selectHeader.classList.contains('fixed-top')) return;
+    window.scrollY > 100 ? selectBody.classList.add('scrolled') : selectBody.classList.remove('scrolled');
+  }
+
+  document.addEventListener('scroll', toggleScrolled);
+  window.addEventListener('load', toggleScrolled);
